01633d376daa4a1532bc4885009460c195372863
TyagoJ <tiago.silva.0567@gmail.com>
2024-11-01 15:54:30 -0300

    Nova demanda

--- git.py
+++ git.py
@@ -1,3 +1,16 @@
 print('Olá, mundo')
 
 print('teste')
+
+
+
+while True:
+    n = input('Digite um número: ')
+    n = int(n)
+
+    if n%2 == 0:
+        print('Fim do progrma')
+
+        break
+
+  
